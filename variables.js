85b637d092c7b7e25fcc676e11e2ecbeb51a9363
Isarel Getu <israelgetu101@gmail.com>
2022-02-11 13:09:45 -0600

    solved problem Q3

--- variables.js
+++ variables.js
@@ -14,6 +14,7 @@ console.log(faveNum);
 /* Create a variable called 'lovesCode' and assign it a boolean value. */
 
 //CODE HERE
+let lovesCode = true;
 
 //////////////////PROBLEM 4////////////////////
 /*
